f4b40a7dd67c57c9146e3e23d141f33a405629d4
diaoweijin <1904609060@qq.com>
2019-08-21 22:50:23 +0800

    000

---
+++ HeatingFeeSystem09/src/main/java/com/example/demo/HeatingFeeSystem09Application.java
@@ -0,0 +1,13 @@
+package com.example.demo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class HeatingFeeSystem09Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(HeatingFeeSystem09Application.class, args);
+	}
+
+}
