7a14808d444540d7c2c839c2355226ec6db92033
chenwujie <Cwj@DESKTOP-P5VAD3O>
2019-08-23 13:36:36 +0800

    neigh

--- HeatingFeeSystem09/src/main/java/cn/jyu/heatingfeesystem/mapper/NeighbourhoodMapper.java
+++ HeatingFeeSystem09/src/main/java/cn/jyu/heatingfeesystem/mapper/NeighbourhoodMapper.java
@@ -8,25 +8,32 @@ import cn.jyu.heatingfeesystem.model.HeatingPriceModel;
 import cn.jyu.heatingfeesystem.model.NeighbourhoodModel;
 
 /**
- *  小区信息表接口：接口方法包括增加一条小区信息、删除小区信息、修改小区信息、
- *  查询所有的小区信息、通过编号查询小区信息。
-* @author 王得志
-*
-*/
+ * 小区信息表接口：接口方法包括增加一条小区信息、删除小区信息、修改小区信息、 查询所有的小区信息、通过编号查询小区信息。
+ * 
+ * @author 王得志
+ *
+ */
 
 public interface NeighbourhoodMapper {
-			//增加一条小区信息
-			public void insert(NeighbourhoodModel neighbourhood);
-			//删除小区信息
-			public void delete(int hooddNo);
-			//修改小区信息
-			public void update(NeighbourhoodModel neighbourhood);
-			//查询所有的小区信息
-			public List<NeighbourhoodModel> selectAll();
-			//通过编号查询小区信息
-			public NeighbourhoodModel selectByTypeNo(int hooddNo);
-			// 查询所有小区的总数
-			public int selectCountByAll() throws Exception;
-			//取得所有小区列表,分页模式
-			public List<NeighbourhoodModel> selectListByAllWithPage(@Param("start") int start,@Param("rows") int rows) throws Exception;
+	// 增加一条小区信息
+	public void insert(NeighbourhoodModel neighbourhood);
+
+	// 删除小区信息
+	public void delete(int hooddNo);
+
+	// 修改小区信息
+	public void update(NeighbourhoodModel neighbourhood);
+
+	// 查询所有的小区信息
+	public List<NeighbourhoodModel> selectAll();
+
+	// 通过编号查询小区信息
+	public NeighbourhoodModel selectByHoodNo(int hoodNo);
+
+	// 查询所有小区的总数
+	public int selectCountByAll() throws Exception;
+
+	// 取得所有小区列表,分页模式
+	public List<NeighbourhoodModel> selectListByAllWithPage(@Param("start") int start, @Param("rows") int rows)
+			throws Exception;
 }
